aadd6f312405ba45a39da9024b24ed1d93913737
bluecoder2003 <neelakshidas2003@gmail.com>
2024-08-22 13:59:44 +0530

    feat: :sparkles: contact added

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -18,7 +18,7 @@ const Page: React.FC = () => {
         <div className="text-lg md:text-xl lg:text-5xl text-center text-bglight">
           Me in a nutshell
         </div>
-        <div className="flex flex-col items-center text-lg md:text-xl lg:text-xl py-10 text-center text-[#746D80] px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-col items-center text-lg md:text-xl lg:text-xl py-10 text-center text-[#746D80] px-4 sm:px-6 lg:px-40 ">
           Design has always been in my DNA. As a kid, you would find me dabbling my hands in paint and making random abstract art, and I grew to like how technology became a very integral part of expressing our creativity over the years. I stumbled upon UX/UI, which was a perfect combination of both. I look forward to my journey in this field after transitioning from a non-design background.
         </div>
       </div>
@@ -31,32 +31,33 @@ const Page: React.FC = () => {
 
       {/* Experiences Section */}
       <div className="space-y-4 py-10 px-4 sm:px-6 lg:px-8">
-        <div className="text-lg md:text-xl font-semibold lg:text-xl text-center gradient-text">
-          MY EXPERIENCES
+  <div className="text-lg md:text-xl font-semibold lg:text-xl text-center gradient-text">
+    MY EXPERIENCES
+  </div>
+  <div className="text-lg md:text-xl lg:text-5xl text-center text-bglight mb-8 md:mb-12 lg:mb-16">
+    Where I’ve been Employed
+  </div>
+  <div className="flex flex-col gap-10">
+    {[1, 2, 3].map((item, index) => (
+      <div key={index} className="flex flex-col mt-10  md:flex-row gap-6">
+        <div className="flex flex-col flex-1 items-center md:items-start">
+          <div className="text-lg md:text-xl font-normal lg:text-xl text-bglight text-center md:text-left">
+            Product Designer Intern,{" "}
+            <span className="gradient-text">Vexio</span>
+          </div>
+          <div className="text-lg md:text-xl font-normal italic lg:text-xl text-bglight text-center md:text-left">
+            Nov 2023 - May 2024 / Bangalore, India
+          </div>
         </div>
-        <div className="text-lg md:text-xl lg:text-5xl text-center text-bglight">
-          Where I’ve been Employed
-        </div>
-        <div className="flex flex-col gap-10 md:gap-14">
-          {[1, 2, 3].map((item, index) => (
-            <div key={index} className="flex flex-col md:flex-row gap-6">
-              <div className="flex flex-col flex-1">
-                <div className="text-lg md:text-xl font-normal lg:text-xl text-bglight text-center flex flex-row">
-                  Product Designer Intern, <div className="gradient-text">Vexio</div>
-                </div>
-                <div className="text-lg md:text-xl font-normal italic lg:text-xl text-bglight text-center">
-                  Nov 2023 - May 2024 / Bangalore, India
-                </div>
-              </div>
-              <div className="flex flex-col flex-1">
-                <div className="text-lg md:text-xl lg:text-xl text-left text-bglight max-w-full md:max-w-md break-words px-4">
-                  Worked as a freelance designer for the EB website, including UX/UI Design and branding.
-                </div>
-              </div>
-            </div>
-          ))}
+        <div className="flex flex-col flex-1">
+          <div className="text-lg md:text-xl lg:text-xl text-left text-bglight max-w-full md:max-w-md break-words px-4">
+            Worked as a freelance designer for the EB website, including UX/UI Design and branding.
+          </div>
         </div>
       </div>
+    ))}
+  </div>
+</div>
     </div>
   )
 }
--- src/components/custom/Footer.tsx
+++ src/components/custom/Footer.tsx
@@ -1,102 +1,28 @@
-import React, { useEffect } from "react";
+import React from "react";
 import ReusableTextComponent from "@/components/custom/ReusableTextComponent";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
-const extProps = [
-  {
-    topText: "LET’S CONNECT",
-    middleText: "Get in Touch",
-    bottomText: "We would love to hear from you",
-  },
-];
 
 const Footer: React.FC = () => {
-  useEffect(() => {
-    AOS.init({
-      duration: 500, // Duration of animations
-      easing: 'ease-in-out', // Easing function
-      once: true, // Whether animation should happen only once - while scrolling down
-    });
-  }, []);
-
   return (
-    <footer className="text-white py-8 bg-black">
-      <div className="container mx-auto px-4">
-        <div className="text-center mb-6" data-aos="fade-up">
-          {extProps.map((props, index) => (
-            <ReusableTextComponent
-              key={index}
-              topText={props.topText}
-              middleText={props.middleText}
-              bottomText={props.bottomText} // Add AOS attribute for animation
-            />
-          ))}
-        </div>
-
-        <form className="w-full max-w-xl mx-auto px-4 md:px-6">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
-            <input
-              type="text"
-              placeholder="Enter your name"
-              className="w-full px-4 py-3 rounded-lg bg-black border-2 border-[#332F3C] placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-[#28133C]"
-              // data-aos="fade-up" // Add AOS attribute for animation
-            />
-            <input
-              type="email"
-              placeholder="Enter your mail"
-              className="w-full px-4 py-3 rounded-lg bg-black border-2 border-[#332F3C] placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-[#28133C]"
-              // data-aos="fade-up" // Add AOS attribute for animation
-            />
+    <footer className="text-bglight py-8 bg-black">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <hr className="border-t border-[#746D80] mb-8" />
+        <div className="flex flex-col sm:flex-row justify-between w-full mt-8 text-[#746D80] text-sm">
+          <div className="flex flex-col sm:flex-row justify-between gap-6 order-1 sm:order-1">
+            <div className="flex items-center">
+              LINKEDIN
+              <img src="/assets/arrow.svg" alt="Arrow" className="ml-2 h-2 w-2" />
+            </div>
+            <div className="flex items-center">
+              GITHUB
+              <img src="/assets/arrow.svg" alt="Arrow" className="ml-2 h-2 w-2" />
+            </div>
+            <div className="flex items-center">
+              DRIBBBLE
+              <img src="/assets/arrow.svg" alt="Arrow" className="ml-2 h-2 w-2" />
+            </div>
           </div>
-          <textarea
-            placeholder="Enter your message"
-            className="w-full px-4 py-3 rounded-lg bg-black border-2 border-[#332F3C] placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-[#28133C] h-24 mb-4"
-            // data-aos="fade-up" // Add AOS attribute for animation
-          ></textarea>
-          <button
-            type="submit"
-            className="w-full py-3 rounded-lg bg-[#28133C] hover:bg-purple-900 transition-colors"
-            // data-aos="fade-up" // Add AOS attribute for animation
-          >
-            Send Message
-          </button>
-        </form>
-
-        <div className="flex justify-center space-x-3 md:space-x-4 mt-8">
-          {/* Social Media Icons */}
-          <a
-            href="#"
-            className="p-2 bg-[#081225] border border-[#262626] rounded-full hover:border-purple-400 transition-colors"
-            // data-aos="fade-up" // Add AOS attribute for animation
-          >
-            <img src="/assets/dribbble.svg" alt="Dribbble" className="w-5 h-5" />
-          </a>
-          <a
-            href="#"
-            className="p-2 bg-[#081225] border border-[#262626] rounded-full hover:border-purple-400 transition-colors"
-            // data-aos="fade-up" // Add AOS attribute for animation
-          >
-            <img src="/assets/github.svg" alt="Github" className="w-5 h-5" />
-          </a>
-          <a
-            href="#"
-            className="p-2 bg-[#081225] border border-[#262626] rounded-full hover:border-purple-400 transition-colors"
-            // data-aos="fade-up" // Add AOS attribute for animation
-          >
-            <img src="/assets/linkedin.svg" alt="Linkedin" className="w-5 h-5" />
-          </a>
-          <a
-            href="#"
-            className="p-2 bg-[#081225] border border-[#262626] rounded-full hover:border-purple-400 transition-colors"
-            // data-aos="fade-up" // Add AOS attribute for animation
-          >
-            <img src="/assets/instagram.svg" alt="Instagram" className="w-5 h-5" />
-          </a>
+          <p className="mb-4 mt-4 sm:mb-0 sm:mt-0 order-2 sm:order-2">© 2024 Neelakshi Das</p>
         </div>
-      </div>
-      <div className="flex justify-center w-full mt-8 text-[#746D80] text-sm">
-        <p>© 2024 Neelakshi Das</p>
       </div>
     </footer>
   );
